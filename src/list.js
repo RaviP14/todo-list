85b1e0fd6bc3563f99bf7c6f033c9e6110e5158d
RaviP14 <ravi.patel14@outlook.com>
2021-07-04 16:31:29 +0100

    Added info & complete buttons to todo's.

--- src/list.js
+++ src/list.js
@@ -7,10 +7,36 @@ const currentTodo = (() => {
         for (let i = domElements.tableTodo.rows.length; i < items.arrInbox.length; i++) {
             let row = domElements.tableTodo.insertRow(i)
             row.setAttribute('data-key', i)
-            for (let j = 0; j < Object.keys(items.arrInbox[i]).length; j++) {
+            for (let j = 0; j < Object.keys(items.arrInbox[i]).length - 1; j++) {
                 let val = row.insertCell()
                 val.textContent = Object.values(items.arrInbox[i])[j];
             }
+
+            let infoBtn = document.createElement('button');
+            infoBtn.textContent = 'i'
+
+            let info = document.createElement('span');
+            info.textContent = items.arrInbox[i].description 
+            info.style.display = 'none'
+
+            let val3 = row.insertCell();
+            val3.appendChild(infoBtn);
+            val3.appendChild(info);
+            infoBtn.addEventListener('mouseover', () => {
+                info.style.display = 'block';
+            })
+            infoBtn.addEventListener('mouseout', () => {
+                info.style.display = 'none'
+            })
+
+            let completeBtn = document.createElement('button');
+            completeBtn.textContent = 'Complete'
+            let val2 = row.insertCell()
+            val2.appendChild(completeBtn)
+            completeBtn.addEventListener('click', () => {
+                items.deleteTodo(i);
+                domElements.tableTodo.deleteRow(i);
+            })
         }
     }
     return {
