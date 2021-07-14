f6e372ca28e28f92b6c775cd58a945e580abf6ef
RaviP14 <ravi.patel14@outlook.com>
2021-07-14 20:19:54 +0100

    Added logic to this week button.

--- src/index.js
+++ src/index.js
@@ -2,55 +2,72 @@ import { items } from './item'
 import{ domElements } from './interface'
 import { currentTodo } from './list'
 import { format } from 'date-fns'
-/*items.addProject('hello','square')
-items.arrProjects[0].addToList('hello','sammy','11','low')
-console.log(items.arrProjects[0].array) */
 
-domElements.btnAddTodo.addEventListener('click', () => {
-    if (domElements.formTodo.length === 0) {
-        domElements.buildFormTodo(domElements.tableTodo , items.arrInbox)
-        domElements.formDiv.style.display = 'block'
-    } else if (domElements.formDiv.style.display === 'none') {
-        domElements.formDiv.style.display = 'block'
-    }
-})
-
-domElements.btnAddProject.addEventListener('click', () => {
-    if (domElements.projectForm.length === 0) {
-        domElements.buildFormProject()
-    } else if (domElements.projectFormDiv.style.display === 'none') {
-        domElements.projectFormDiv.style.display = 'block'
-    }
-})
-
-domElements.todoBtn.addEventListener('click', () => {
-    for (let i = 0; i < domElements.tableTodo.rows.length; i++) {
-        domElements.tableTodo.rows[i].style.display = 'block';
-    }
-})
-
-domElements.inboxBtn.addEventListener('click', () => {
-    for (let j = 0; j < domElements.tableTodo.rows.length; j++) {
-        domElements.tableTodo.rows[j].style.display = 'none';
-        if (domElements.tableTodo.rows[j].attributes.value.nodeValue === 'Inbox') {
-            domElements.tableTodo.rows[j].style.display = 'block';
+const homepage = (() => {
+    domElements.btnAddTodo.addEventListener('click', () => {
+        if (domElements.formTodo.length === 0) {
+            domElements.buildFormTodo(domElements.tableTodo , items.arrInbox)
+            domElements.formDiv.style.display = 'block'
+        } else if (domElements.formDiv.style.display === 'none') {
+            domElements.formDiv.style.display = 'block'
         }
-    }
-})
-
-domElements.todayBtn.addEventListener('click', () => {
-    let thisDay = new Date()
-    let day1 = String(thisDay.getDate())
-    let month1 = String(thisDay.getMonth())
-    let year1 = thisDay.getFullYear()
-    let today = format(thisDay, 'dd-MM-yyyy')
-    for (let j = 0; j < domElements.tableTodo.rows.length; j++) {
-        domElements.tableTodo.rows[j].style.display = 'none';
-        if (domElements.tableTodo.rows[j].attributes[0].nodeValue === today) {
-            domElements.tableTodo.rows[j].style.display = 'block';
+    })
+    
+    domElements.btnAddProject.addEventListener('click', () => {
+        if (domElements.projectForm.length === 0) {
+            domElements.buildFormProject()
+        } else if (domElements.projectFormDiv.style.display === 'none') {
+            domElements.projectFormDiv.style.display = 'block'
         }
-    }
-})
-
-/* access each project todo array example items.arrProjects[0].array */
-console.log('todo')
+    })
+    
+    domElements.todoBtn.addEventListener('click', () => {
+        for (let i = 0; i < domElements.tableTodo.rows.length; i++) {
+            domElements.tableTodo.rows[i].style.display = 'block';
+        }
+    })
+    
+    domElements.inboxBtn.addEventListener('click', () => {
+        for (let j = 0; j < domElements.tableTodo.rows.length; j++) {
+            domElements.tableTodo.rows[j].style.display = 'none';
+            if (domElements.tableTodo.rows[j].attributes.value.nodeValue === 'Inbox') {
+                domElements.tableTodo.rows[j].style.display = 'block';
+            }
+        }
+    })
+    
+    domElements.todayBtn.addEventListener('click', () => {
+        let thisDay = new Date()
+        /*let day1 = String(thisDay.getDate())
+        let month1 = String(thisDay.getMonth())
+        let year1 = thisDay.getFullYear()*/
+        let today = format(thisDay, 'dd-MM-yyyy')
+        for (let j = 0; j < domElements.tableTodo.rows.length; j++) {
+            domElements.tableTodo.rows[j].style.display = 'none';
+            if (domElements.tableTodo.rows[j].attributes[0].nodeValue === today) {
+                domElements.tableTodo.rows[j].style.display = 'block';
+            }
+        }
+    })
+    
+    domElements.thisWeekBtn.addEventListener('click', () => {
+        let thisDay = new Date()
+        let firstDay = thisDay.getDate() - thisDay.getDay()
+        let first = new Date(thisDay.setDate(firstDay))
+        let starts = format(thisDay, 'dd,MM,yyyy')
+    
+        let endDay = new Date()
+        let lastDay = firstDay + 6
+        let last = new Date(endDay.setDate(lastDay))
+        let ends = format(endDay, 'dd,MM,yyyy')
+        
+        for(let j = 0; j < domElements.tableTodo.rows.length; j++) {
+            domElements.tableTodo.rows[j].style.display = 'none';
+            let current = domElements.tableTodo.rows[j].attributes[0].nodeValue
+            
+            if (current > starts && current < ends) {
+                domElements.tableTodo.rows[j].style.display = 'block';
+            }
+        }
+    })
+})()
